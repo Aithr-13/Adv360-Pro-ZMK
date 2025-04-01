a435616bd76e7a548495460905da1620fb566195
hd2117 <hd2117@lab24.co>
2025-04-01 09:12:09 -0400

    new attempt

--- config/boards/arm/adv360/board.cmake
+++ config/boards/arm/adv360/board.cmake
@@ -7,3 +7,13 @@ board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 
 include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+# Inside config/boards/arm/adv360/board.cmake
+
+# Find a suitable place, often near other zephyr_library_sources() calls
+
+# Add this block:
+if(CONFIG_ZMK_BEHAVIOR_ALT_TAB)
+  zephyr_library_sources(alt-tab.c) # If alt-tab.c is directly in this folder
+  # If you put it in a subdirectory like 'src':
+  # zephyr_library_sources(src/alt-tab.c)
+endif()
